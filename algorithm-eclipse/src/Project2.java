ef8201333947dffc76d1414eaac87be643fe4742
Justin Ong <justin.ong.97@gmail.com>
2020-10-30 11:15:14 +0800

    Clean up handling of cli arguments

--- algorithm-eclipse/src/Project2.java
+++ algorithm-eclipse/src/Project2.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 public class Project2 {
 	public static void main(String[] args) throws Exception {
@@ -7,17 +6,33 @@ public class Project2 {
 			System.out.println("Usage: app <path to edges> <path to hospitals> <num of nearest paths>");
 			return;
 		}
-		
-		int iterationCount = 1;
-		for (String s : args) {
-			if (s.startsWith("-i")) {
-				iterationCount = Integer.parseInt(s.substring(2));
-			}
-		}
 
 		String edgePath = args[0];
 		String hospitalPath = args[1];
 		int numPaths = Integer.parseInt(args[2]);
+		
+		int iterationCount = 1;
+		String outPath = null;
+		
+		for (int i = 3; i < args.length; i++) {
+			String s = args[i];
+			if (s.startsWith("-i") || s.startsWith("--iterations")) {
+				if ((i+1) >= args.length || args[i+1].startsWith("-")) {
+					System.out.println("Expected value after -i/--iterations");
+					return;
+				}
+				
+				iterationCount = Integer.parseInt(args[i+1]);
+				i++;
+			} else if (s.startsWith("-o") || s.startsWith("--out")) {
+				if ((i+1) >= args.length || args[i+1].startsWith("-")) {
+					System.out.println("Expected value after -o/--out");
+					return;
+				}
+				
+				outPath = args[i+1];
+			}
+		}
 
 		FileReader fr = new FileReader(new File(edgePath));
 		BufferedReader br = new BufferedReader(fr);
@@ -70,24 +85,17 @@ public class Project2 {
 		
 		System.out.printf("Took %dns on average to exec %d iterations\n", totalExecTime / executionCount, executionCount);
 		
-		if (args.length >= 4) {
-			String outPath = null;
-			for (int i = 3; i < args.length; i++) {
-				if (args[i].startsWith("-i")) continue;
-				
-				outPath = args[3];
+			
+		if (outPath != null) {
+			System.out.printf("Writing output to %s\n", outPath);
+			BufferedWriter writer = new BufferedWriter(new FileWriter(outPath));
+			
+			for (Vertex v : search.getVertexes()) {
+			    writer.write(v.toString());
+			    writer.write('\n');
 			}
 			
-			if (outPath != null) {
-				BufferedWriter writer = new BufferedWriter(new FileWriter(outPath));
-				
-				for (Vertex v : search.getVertexes()) {
-				    writer.write(v.toString());
-				    writer.write('\n');
-				}
-				
-			    writer.close();
-			}
+		    writer.close();
 		}
 	}
 }
